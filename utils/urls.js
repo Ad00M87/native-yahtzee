a48f10a76124f915e0bacb2fc9605c4478848d19
Adam Kimball <adamchasekimball@gmail.com>
2018-03-28 11:16:11 -0600

    initial commit

---
+++ utils/urls.js
@@ -0,0 +1,5 @@
+# If you want to use our live public server use this base url
+# https://dpl-code-on-yahtzee-server.herokuapp.com/
+
+export const AUTH_URL = 'https://dpl-code-on-yahtzee-server.herokuapp.com/auth'
+export const API_URL = 'https://dpl-code-on-yahtzee-server.herokuapp.com/api'
